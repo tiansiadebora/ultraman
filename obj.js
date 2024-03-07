657f85ef4778b67dd797c9508acd17f5d4a7096c
Tiansi Ade Bora <adetiansi.30@gmail.com>
2024-03-07 09:12:20 +0700

    isi file obj

---
+++ obj.js
@@ -0,0 +1,17 @@
+const ultramantObj = {
+  name: "tiga",
+  height: 100,
+  isDead: false,
+  address: {
+    planet: {
+      name: "namex",
+      moon: [88390, 23456],
+    },
+    galaxy: "andromeda 12",
+  },
+  weapons: ["sword", "shield", "lasso"],
+  attack: () => {
+    console.log("sword attack!");
+  },
+  beam: (number) => console.log("spacium ray beam " + number + "%"),
+};
